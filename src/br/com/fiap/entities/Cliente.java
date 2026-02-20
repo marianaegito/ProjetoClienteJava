16e1e6f15dba72fc966f2ca55ee4317714fb7f91
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-20 08:25:24 -0300

    Primeiro Commit

---
+++ src/br/com/fiap/entities/Cliente.java
@@ -0,0 +1,39 @@
+package br.com.fiap.entities;
+
+public class Cliente {
+    private String nome, cpf;
+    private int idade;
+    private double altura;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
