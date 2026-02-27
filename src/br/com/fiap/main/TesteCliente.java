2d9beed4c068dff68f4e873e8e5c004286147050
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 09:56:04 -0300

    Adicionando mÂétodos estáticos de JOptionPane

--- src/br/com/fiap/main/TesteCliente.java
+++ src/br/com/fiap/main/TesteCliente.java
@@ -1,10 +1,27 @@
 package br.com.fiap.main;
+import  javax.swing.*;
 
 import br.com.fiap.entities.Cliente;
 
 import javax.swing.*;
 
+
 public class TesteCliente {
+
+    // String
+    static  String texto(String j){
+        return JOptionPane.showInputDialog(j);
+    }
+
+    // Int
+    static int inteiro(String j){
+        return  Integer.parseInt(JOptionPane.showInputDialog(j));
+    }
+
+    // Double
+    static double real(String j){
+        return Double.parseDouble((JOptionPane.showInputDialog(j)));
+    }
     // psvm
     public static void main(String[] args) {
 
@@ -12,10 +29,10 @@ public class TesteCliente {
         // Instanciar objetos
         Cliente ObjCliente = new Cliente();
 
-        ObjCliente.setNome(JOptionPane.showInputDialog("Digite o seu nome: "));
-        ObjCliente.setCpf(JOptionPane.showInputDialog("Digite o seu CPF: "));
-        ObjCliente.setIdade(Integer.parseInt(JOptionPane.showInputDialog("Informe a sua idade: ")));
-        ObjCliente.setAltura(Double.parseDouble(JOptionPane.showInputDialog("Informe a sua altura: ")));
+        ObjCliente.setNome(texto("Digite o seu nome: "));
+        ObjCliente.setCpf(texto("Digite o seu CPF: "));
+        ObjCliente.setIdade(inteiro("Informe a sua idade: ")));
+        ObjCliente.setAltura(real("Informe a sua altura: ")));
 
         // sout
         JOptionPane.showMessageDialog(null,
